ccff359748ce79c646eea88f4b7f8586d3ebb17d
yosafatdeny <yosafatdenysatrio@gmail.com>
2021-03-10 10:32:44 +0700

    add from wsl

--- bash.sh
+++ bash.sh
@@ -3,3 +3,4 @@
 echo "file ini dibuat menggunakan github webhook" > myfile.txt
 echo "'$BUILD_NUMBER' + '$USER'" >> myfile.txt
 echo "hasil dari trigger github" >> myfile.txt
+echo 'tambah baris lagi' >> myfile.txt
